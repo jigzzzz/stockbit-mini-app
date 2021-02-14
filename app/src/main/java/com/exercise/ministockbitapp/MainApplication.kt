e9eba1e07eec4abc8c066b2d7c29ceb9b30dc850
Anggi Maisa Hadaya <anggimhadaya@gmail.com>
2021-02-14 14:32:22 +0700

    Initial Commit

---
+++ app/src/main/java/com/exercise/ministockbitapp/MainApplication.kt
@@ -0,0 +1,7 @@
+package com.exercise.ministockbitapp
+
+import android.app.Application
+
+class MainApplication : Application() {
+
+}
---
+++ app/src/main/java/com/exercise/ministockbitapp/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.exercise.ministockbitapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.exercise.ministockbitapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
